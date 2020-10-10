7f9d1d67a4559b14393417b85511be89035f3cc2
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2020-10-10 19:02:29 +0200

    should redirect to another page, need fix

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+// const registerBtn = 
+function redirectToRegister (){
+    window.location.href= '/php/register.php';
+}
+// registerBtn.onclick = function(){
+    // location.href="/register.php";
+// };
